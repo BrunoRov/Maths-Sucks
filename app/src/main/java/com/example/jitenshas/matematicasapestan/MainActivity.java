3659b6b6a4e918216ad72201d1be3dda4f368e13
bruno <bruno@localhost.localdomain>
2017-06-04 05:57:41 -0500

    Primer Commit

---
+++ app/src/main/java/com/example/jitenshas/matematicasapestan/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.jitenshas.matematicasapestan;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView totalTextView ;
+    EditText ptext;
+    EditText ntext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        ptext = (EditText) findViewById(R.id.ptext);
+        ntext = (EditText) findViewById(R.id.ntext);
+
+        Button  calcular = (Button) findViewById(R.id.calcular);
+        calcular.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                float porcentaje =Float.parseFloat( ptext.getText().toString());
+                float numero = Float.parseFloat(ntext.getText().toString());
+                float dec = porcentaje/100;
+                float total = numero * dec;
+                totalTextView.setText(Float.toString(total));
+
+
+
+            }
+        });
+
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
